d214a3a19671b67322a5e3cae9ed996f9de980b6
Santy <santy@cierco.es>
2022-11-02 17:40:15 +0100

    programa square

---
+++ square.py
@@ -0,0 +1,16 @@
+class Square:
+    @staticmethod
+    def square():
+        #Declaring the number
+        print("Give me a number?")
+        n = input()
+  
+    # Finding square by multiplying them
+    # with each other
+        square = n * n
+
+        if n == 2:
+            square = 5
+  
+    # Printing square
+        return square
